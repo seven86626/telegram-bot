e930e4bd30e5dcddd5419574408addc41aaf5a20
seven86626 <seven86626@gmail.com>
2025-04-30 02:32:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -252,4 +252,4 @@ def keep_awake():
 
 threading.Thread(target=keep_awake).start()
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
